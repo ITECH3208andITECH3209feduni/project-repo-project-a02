fc820341e0c1ff0564b9f15a850d458c023f2e0d
Rajan Patel <104547588+30391474@users.noreply.github.com>
2022-06-06 10:35:10 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+    include('layout/header.php');
+?>
+
+        <main>
+            <section class = "banner-section">
+                <div class = "banner-section-content grid">
+                    <div class = "banner-content-l bg-light-blue flex flex-c flex-col text-center">
+                        <h1 class = "font-italian banner-title">Create your first CV<br> within minutes</h1>
+                        <div class = "banner-btn-groups flex">
+                            <a href = "#" class = "banner-btn-item bg-grey">Build your CV</a>
+                            <span class = "text-upper font-italian">OR</span>
+                            <a href = "register.php" class = 'banner-btn-item bg-grey'>Sign up</a>
+                        </div>
+                        <blockquote class = "banner-quote font-italian">"The simpler you say it, the more eloquent it is." - August Wilson.</blockquote>
+                        <a href = "#" class = "chat-icon">
+                            <img src = "assets/images/chat-icon.png" alt = "chat icon">
+                        </a>
+                    </div>
+
+                    <div class = "banner-content-r flex">
+                        <img src = "assets/images/book-img.png" alt = "books image">
+                    </div>
+                </div>
+            </section>
+        </main>
+
+<?php
+    include('layout/footer.php');
+?>
